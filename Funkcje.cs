10855509180f6e135d3a2020a8e2175aa7ffaf8e
Pawel Jaskula <pj.jaskula@gmail.com>
2017-05-13 14:19:28 +0200

    Funkcje
    Klasa funkcje

---
+++ Funkcje.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppCalkowanieNumeryczne
+{
+    class Funkcje
+    {
+        public static double wielomian(int[] tab, double x)
+        {
+            double rezultat = tab[tab.Length - 1];
+            double potega = 1;
+            for (int i = tab.Length - 2; i >= 0; i--)
+            {
+                potega *= x;
+                rezultat += tab[i] * potega;
+            }
+            return rezultat;
+        }
+    }
+}
